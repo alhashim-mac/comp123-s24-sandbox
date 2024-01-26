ef4326247200caba802d3360ac46bc8c0eb40402
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-26 00:51:00 -0600

    Prep for Fri 1/26

---
+++ prep/drawRectangle.py
@@ -0,0 +1,17 @@
+import turtle
+
+screen1 = turtle.Screen()
+turtle1 = turtle.Turtle()
+
+turtle1.forward(100)
+turtle1.left(90)
+
+turtle1.forward(200)
+turtle1.left(90)
+
+turtle1.forward(100)
+turtle1.left(90)
+
+turtle1.forward(200)
+
+screen1.exitonclick()
