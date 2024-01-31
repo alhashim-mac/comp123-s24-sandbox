f87ba7b297f9104f8f99ef5c035ec0bcc3b7cabd
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-31 13:35:50 -0600

    Class demo

--- Lec06/decorate.py
+++ Lec06/decorate.py
@@ -1,6 +1,12 @@
-def decorate(str):
-    print("***", str, "***")
+def printStarts(n):
+    for i in range(n+8):
+        print("#", end="")
+    print()
 
+def decorate(str):
+    printStarts(len(str))
+    print("***", str, "***")
+    printStarts(len(str))
 
 decorate("Jonah")
 decorate("July June")
