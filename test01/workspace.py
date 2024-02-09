f2ac6aaab2deddfbd33dadf3f817709982c88552
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-02-09 14:11:17 -0600

    Lec09 Demo

---
+++ test01/workspace.py
@@ -0,0 +1,13 @@
+import math
+# import random
+#
+# l = [10, 15, 16, 20]
+# random.seed()
+# r = random.choice(l)
+#
+# n = random.randint(0, len(l))
+# print(l[n])
+#
+# print(r)
+
+print(math.factorial(10))
