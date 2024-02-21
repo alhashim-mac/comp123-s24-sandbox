bcfadaca02c2176671af856afbc8115b453d4376
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-02-21 14:19:09 -0600

    Update questions.py

--- lec15_strings/questions.py
+++ lec15_strings/questions.py
@@ -6,15 +6,29 @@ different, each character was assigned a numeric value and based and strings
 are compared based on these values, see https://www.ascii-code.com/ for the
 values assigned to each character when using the ascii encoding.
 """
+
+# print('apple' < 'orange')
+
 # find out the characters for the first values from 0-200
-for val in range(0, 200):
-    print('{} -> {}'.format(val, chr(val)))
+# for val in range(0, 200):
+#     print('{} -> {}'.format(val, chr(val)))
+    # print(val, "->", chr(val))
 
 # the reverse can also be done
-for ch in 'ABab123!#':
-    print('{} -> {}'.format(ch, ord(ch)))
-
-
-# slice operator
+# for ch in 'ABab123!#':
+#     print('{} -> {}'.format(ch, ord(ch)))
+#
+#
+# # slice operator
 s = "This is a test for the slice operator"
-print(s[0: 6])  # get the slice that starts at 0 (inclusive) and ends at 6 (exclusive)
+print(s[6: 10] + s[5:9])  # get the slice that starts at 0 (inclusive) and ends at 6 (exclusive)
+
+
+
+
+
+
+
+
+
+
