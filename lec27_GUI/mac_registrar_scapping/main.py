65de1081610440e8740661759cbc94b24bb4a063
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-04-08 08:11:08 -0500

    Leaving home

---
+++ lec27_GUI/mac_registrar_scapping/main.py
@@ -0,0 +1,12 @@
+"""
+Inspirational Tutorial: https://www.youtube.com/watch?v=1PCWwK0AsE0&t=12s
+
+Required Packages:
+- httpx: alternatives: requests
+- selectolax: HTML parser, alternatives: Beautiful Soup
+"""
+
+import httpx
+from selectolax.parser import HTMLParser
+
+url = ''
